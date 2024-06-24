c143ab1ed55176fff0d993476c0ed1120ab5ce2c
Saitto <syed.abubakar73@gmail.com>
2024-06-24 12:33:12 +0800

    second day challenge

---
+++ Day 2/Part 1.py
@@ -0,0 +1,36 @@
+import re
+
+with open("Day 2\input.txt") as f:
+    fileInput = f.read()
+
+listInput = fileInput.split("\n")
+
+dictItem = {"blue":"0","red":"0","green":"0"}
+
+possibleGame = 0
+
+for game in listInput:
+    dictItem = {"blue":"0","red":"0","green":"0"}
+    print("The current game is "+game)
+    currentGame = re.sub("Game [1-9][0-9]?:|Game 100:","",game)
+    currentTurn = currentGame.split(";")
+    for marble in currentTurn:
+        print("The current turn is "+ marble)
+        color = marble.split(",")
+        for marbleColor in color:
+            print("The current marble: " + marbleColor)
+            if marbleColor.__contains__("blue"):
+                dictItem.update({"blue" : marbleColor.replace("blue","")})
+            elif marbleColor.__contains__("red"):
+                dictItem.update({"red": marbleColor.replace("red","")})
+            else:
+                dictItem.update({"green":marbleColor.replace("green","")})
+            
+            if int(dictItem["red"]) <= 12 and int(dictItem["blue"]) <= 14 and int(dictItem["green"]) <= 13:
+                temp = game.replace(currentGame,"")
+                temp = temp.replace("Game","")
+                temp = temp.replace(":","")
+                temp.strip
+                possibleGame = possibleGame + int(temp)
+           
+print("The total is "+str(possibleGame))
