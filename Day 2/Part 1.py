d342fbf0ff68a77f4b17387d2b173fa992d9446b
Saitto <syed.abubakar73@gmail.com>
2024-06-24 14:09:05 +0800

    update code logic for part 1 day 2

--- Day 2/Part 1.py
+++ Day 2/Part 1.py
@@ -7,18 +7,18 @@ listInput = fileInput.split("\n")
 
 dictItem = {"blue":"0","red":"0","green":"0"}
 
-possibleGame = 0
+possibleGame = 5050
 
 for game in listInput:
     dictItem = {"blue":"0","red":"0","green":"0"}
-    print("The current game is "+game)
+    #print("The current game is "+game)
     currentGame = re.sub("Game [1-9][0-9]?:|Game 100:","",game)
     currentTurn = currentGame.split(";")
     for marble in currentTurn:
-        print("The current turn is "+ marble)
+        #print("The current turn is "+ marble)
         color = marble.split(",")
         for marbleColor in color:
-            print("The current marble: " + marbleColor)
+            #print("The current marble: " + marbleColor)
             if marbleColor.__contains__("blue"):
                 dictItem.update({"blue" : marbleColor.replace("blue","")})
             elif marbleColor.__contains__("red"):
@@ -26,11 +26,14 @@ for game in listInput:
             else:
                 dictItem.update({"green":marbleColor.replace("green","")})
             
-            if int(dictItem["red"]) <= 12 and int(dictItem["blue"]) <= 14 and int(dictItem["green"]) <= 13:
-                temp = game.replace(currentGame,"")
-                temp = temp.replace("Game","")
-                temp = temp.replace(":","")
-                temp.strip
-                possibleGame = possibleGame + int(temp)
+        if int(dictItem["red"]) > 12 or int(dictItem["blue"]) > 14 or int(dictItem["green"]) > 13:
+            temp = game.replace(currentGame,"")
+            print("Not applicable - " + temp)
+            temp = temp.replace("Game","")
+            temp = temp.replace(":","")
+            temp.strip
+            possibleGame = possibleGame - int(temp)
+            break
+
            
 print("The total is "+str(possibleGame))
