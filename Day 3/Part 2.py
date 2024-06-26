fbe75de60a38a9e521f693e9272d2d4be1569c65
Saitto <syed.abubakar73@gmail.com>
2024-06-26 14:49:27 +0800

    update logic part 2

--- Day 3/Part 2.py
+++ Day 3/Part 2.py
@@ -9,22 +9,26 @@ for indexRow, row in enumerate(inputFileRow):
     for indexColumn, column in enumerate(row):
         if column.isdigit() or column != "*":
             continue
-        for currentRow in [indexRow-1,indexRow+1]:
-            for currentColumn in [indexColumn-1,indexColumn+1]:
+        for currentRow in [indexRow-1,indexRow,indexRow+1]:
+            for currentColumn in [indexColumn-1,indexColumn,indexColumn+1]:
                 if currentRow < 0 or currentRow >= len(inputFileRow) or currentColumn < 0 or currentColumn >= len(inputFileRow[currentRow]) or not inputFileRow[currentRow][currentColumn].isdigit():
                     continue
                 while currentColumn > 0 and inputFileRow[currentRow][currentColumn-1].isdigit():
                     currentColumn -= 1
                 coordinateFirstChar.add((currentRow,currentColumn))
+        if len(coordinateFirstChar) < 2:
+            coordinateFirstChar.clear()
         for row, column in coordinateFirstChar:
             fullString = ""
             while column < len(inputFileRow[row]) and inputFileRow[row][column].isdigit():
                 fullString += inputFileRow[row][column]
                 column +=1
             multiplyGearRatio.append(int(fullString))
-        if len(multiplyGearRatio) != 0:
+        if len(multiplyGearRatio) != 0 and len(multiplyGearRatio)%2 ==0:
             productItem = 1
             for item in multiplyGearRatio:
                 productItem = productItem * item
             summationGearRatio.append(productItem)
+            multiplyGearRatio.clear()
+            coordinateFirstChar.clear()
 print(sum(summationGearRatio))
